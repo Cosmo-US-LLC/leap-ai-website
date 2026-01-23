cbf3266a36f7c5a16926152e73fed7a26f2155d8
nomanqadir-cosmoinc <nomanqadir@cosmoinc.co>
2026-01-23 15:29:44 +0500

    remove sections

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -21,7 +21,7 @@ export function Home() {
       <ImplementationJourneySection />
       <CommonQuestions />
       <MeetTheAuthors />
-      <DeliverablesSection />
+      {/* <DeliverablesSection /> */}
       <VideoCallStrategySession />
       <DontJustAdoptAi />
     </>
